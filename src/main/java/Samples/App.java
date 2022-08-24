00fbc73f78d2b29a4ca8348986e97b95d2e3d4be
mvohal <vohals@outlook.com>
2022-08-24 17:49:41 +0000

    Update App.java

--- src/main/java/Samples/App.java
+++ src/main/java/Samples/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World Mr. Manish Vohal!!" );
+        System.out.println( "Hello Hello World Mr. Manish Vohal!!" );
     }
 }
